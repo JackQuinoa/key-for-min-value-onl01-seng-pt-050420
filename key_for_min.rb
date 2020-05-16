ccd145839657947e81614569a45328a257f0f873
Jack Quinoa <jquinoa208@gmail.com>
2020-05-16 23:32:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name = ""
+name = nil 
 lowest_valaue = 1050
   name_hash.collect do |key, value|
     if value < lowest_valaue
